733e6706cb9c08a7f91be7c2175a66605efbeaa4
Nishan46 <nipunanishan46@gmail.com>
2023-04-04 05:04:39 +0530

    eight

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -86,8 +86,8 @@ export async function POST(req:NextRequest) {
 
   const response = await axios.put(source_file_url,{message:`Youtube_playlist${new Date().toLocaleTimeString()}`,content:Bas64Content} , options);
 
-  const db_response = await axios.post(`${db_url}/api/telling`,{name:file_name , url:source_file_url} , {headers: {'Authorization': 'Bearer ' + db_apiKey}})
-  return NextResponse.json({msg:'done' , url:response.data , title:file_name , dbres:db_response.data})
+  // const db_response = await axios.post(`${db_url}/api/telling`,{name:file_name , url:source_file_url} , {headers: {'Authorization': 'Bearer ' + db_apiKey}})
+  return NextResponse.json({msg:'done' , url:response.data , title:file_name})
 }
 
 
